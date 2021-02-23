aa04333b8af288ec345991297285df60091d5bd1
Marlon Strong <tsenane14@gmail.com>
2021-02-23 14:13:13 +0200

    added classname and styles to experienceBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { ExperienceBar } from "./components/ExperienceBar";
 
 import "./styles/global.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <ExperienceBar/>
     </>
   );
 }
